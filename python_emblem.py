e5c7ed3ed4d08e778aedba9c5d4efa7866d99463
Safyire <32968063+Safyire@users.noreply.github.com>
2017-12-05 14:25:32 -0800

    Update python_emblem.py

--- python_emblem.py
+++ python_emblem.py
@@ -5,7 +5,7 @@ import random
 weapon_type = 0
 weapon_type_list = ["sword", "lance", "axe", "bow", "tome"]
 player_hp = random.randint(40, 60)
-player_str = random.randint(10, 20)
+player_str = random.randint(15, 25)
 player_def = random.randint(9, 19)
 enemy_hp = random.randint(40, 60)
 enemy_str = random.randint(10, 20)
@@ -17,14 +17,30 @@ def window_init():
     root.title("Python Emblem")
     # TODO: Replace the placeholder favicon
     root.iconbitmap("favicon.ico")
-    label_title = tkinter.Label(root, text="Welcome to Python Emblem!")
+    label_title = tkinter.Label(root, text="Welcome to Python Emblem!", width=50, height=2)
     how_to_play_button = tkinter.Button(root, text="How to play")
     change_weapon_button = tkinter.Button(root, text="Change weapon")
     fight_button = tkinter.Button(root, text="Fight!")
+
     label_title.grid(row=0, column=0)
     how_to_play_button.grid(row=1, column=0)
     change_weapon_button.grid(row=2, column=0)
     fight_button.grid(row=3, column=0)
+
+
+def how_to_play():
+    # TODO: WIP
+    return 0
+
+
+def change_weapon():
+    # TODO: WIP
+    return 0
+
+
+def fight():
+    # TODO: WIP
+    return 0
 
 
 def main():
